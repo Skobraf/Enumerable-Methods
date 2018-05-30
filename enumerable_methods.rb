4693fab7f302f72e298f2a2dd27a578db2af9f28
Ayoub <skobraf@gmail.com>
2018-05-30 14:11:10 +0100

    added my_any? method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -29,21 +29,40 @@ module Enumerable
     return true
   end
 
+  def my_any?
+    self.my_each do |value|
+      if yield(value)
+        return true
+      end
+    end
+    return false
+  end
 end
-
+# Testing below
+puts "Testing my_each"
 [1,2,3].my_each do |x|
   puts x
 end
 
-[1,2,3].my_each_with_index do |value,index|
-  puts "the value is: #{value} with the index of : #{index}"
+puts "Testing my_each_with_index"
+[1,2,3].my_each_with_index do |value, i|
+  puts "#{value} has index: #{i}."
 end
 
-evenNumbers = [1,2,3,5,6,7,8].my_select do |value|
+puts "Testing my_select"
+evenNumbers = [1,2,3,4,5,6,7,8].my_select do |value|
   value % 2 == 0
 end
+puts evenNumbers
 
 puts "Testing my_all?"
 result = [1,1,1,1].my_all? do |value|
   value == 1
 end
+puts result
+
+puts "Testing my_any?"
+result = [2,2,2,2].my_any? do |value|
+  value == 1
+end
+puts result
