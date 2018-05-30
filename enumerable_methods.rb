d439b5ed66bc997c1ab9bcbbbba14f81164c4744
Ayoub <skobraf@gmail.com>
2018-05-30 13:47:06 +0100

    added my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,7 +1,21 @@
 module Enumerable
   def my_each
     for i in 0...self.length do
-      puts self[i]
+      yield(self[i])
     end
   end
+
+  def my_each_with_index
+    for i in 0...self.length do
+      yield(self[i], i)
+    end
+  end
+end
+
+[1,2,3].my_each do |x|
+  puts x
+end
+
+[1,2,3].my_each_with_index do |value,index|
+  puts "the value is: #{value} with the index of : #{index}"
 end
